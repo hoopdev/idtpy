739b200173a401a2de532c5d039423619aa72cbb
hoopdev <taga.kotaro.62d@st.kyoto-u.ac.jp>
2024-12-26 14:03:22 +0900

    Add DxfAssistant ref to __init__.py

--- idtpy/__init__.py
+++ idtpy/__init__.py
@@ -1,3 +1,4 @@
 from idtpy import designer, model, shapes
+from idtpy.dxfutils import DxfAssistant
 from idtpy.gdsutils import GdsAssistant
-from idtpy.version import __version__, __author__
+from idtpy.version import __author__, __version__
